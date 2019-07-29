c7a9bb805b85dd2cd061aaaf999f3e631b5404cf
Nikola Pavlov <nikolapavlov33@gmail.com>
2019-07-29 09:58:03 +0200

    added containers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './containers/not-found/not-found.component';
+import { HomepageComponent } from './containers/homepage/homepage.component';
+import { ProductpageComponent } from './containers/productpage/productpage.component';
+import { CartpageComponent } from './containers/cartpage/cartpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomepageComponent,
+    ProductpageComponent,
+    CartpageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/containers/cartpage/cartpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cartpage',
+  templateUrl: './cartpage.component.html',
+  styleUrls: ['./cartpage.component.scss']
+})
+export class CartpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
