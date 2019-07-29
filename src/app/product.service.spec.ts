4f0f22fcb2becdb30e6e8d3098b03d3dad0e1fd3
Nikola Pavlov <nikolapavlov33@gmail.com>
2019-07-29 10:46:53 +0200

    added product service

---
+++ src/app/product.service.spec.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  cartList = [];
+  productList = [];
+  productsUrl = './assets/products.json';
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private http: HttpClient
+  ) {}
+
+  getProducts(): Observable<any> {
+    return this.http.get(this.productsUrl);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  addToCart(product: object) {
+    this.cartList.push(product);
+    console.log(this.cartList);
+  }
+}
