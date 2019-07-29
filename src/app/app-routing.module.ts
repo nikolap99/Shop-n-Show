9268501625794cc4d3cdfb535db24ab0ab1a409e
Nikola Pavlov <nikolapavlov33@gmail.com>
2019-07-29 10:36:16 +0200

    added header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomepageComponent } from './containers/homepage/homepage.component';
+import { NotFoundComponent } from './containers/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomepageComponent },
+  /*{ path: 'product/:id', component: ProductComponent },
+  { path: 'cart', component: CartComponent },*/
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './containers/not-found/not-found.component';
-import { HomepageComponent } from './containers/homepage/homepage.component';
-import { ProductpageComponent } from './containers/productpage/productpage.component';
-import { CartpageComponent } from './containers/cartpage/cartpage.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NotFoundComponent,
-    HomepageComponent,
-    ProductpageComponent,
-    CartpageComponent
-  ],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NotFoundComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
