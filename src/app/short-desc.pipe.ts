27a9dad922276e66f33ce96cb31adeba514cfac1
Nikola Pavlov <nikolapavlov33@gmail.com>
2019-07-29 14:03:10 +0200

    changed shortDesc pipe and fixed it to take two arguments

--- src/app/short-desc.pipe.ts
+++ src/app/short-desc.pipe.ts
@@ -4,7 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'shortDesc'
 })
 export class ShortDescPipe implements PipeTransform {
-  transform(value: string, length: number): any {
-    return value.substring(0, length);
+  transform(value: any, ...args: any[]): any {
+    let [length, isShort] = [...args];
+    if (isShort) {
+      return `${value.substring(0, length)}...`;
+    } else if (isShort === false) {
+      return value;
+    }
   }
 }
