5e214e9d357f93d9f0c1188bd70b70e4e4729110
Nikola Pavlov <nikolapavlov33@gmail.com>
2019-08-01 11:38:16 +0200

    added functionality to update cart-box in header when quantity is changed on the cart page

--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../../product';
+import { ProductService } from '../../product.service';
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
@@ -8,7 +9,7 @@ import { Product } from '../../product';
 export class CartItemComponent implements OnInit {
   @Input() product: Product;
   quantity: number;
-  constructor() {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
     let { quantity = 0 } = { ...this.product };
@@ -17,9 +18,17 @@ export class CartItemComponent implements OnInit {
 
   incrementProduct() {
     this.quantity++;
+    this.addToCart(1);
   }
   decrementProduct() {
-    if (this.quantity > 1) this.quantity--;
+    if (this.quantity > 1) {
+      this.quantity--;
+      this.addToCart(-1);
+    }
+  }
+
+  addToCart(count) {
+    this.productService.addToCart(this.product, count);
   }
 
   removeCartItem() {
